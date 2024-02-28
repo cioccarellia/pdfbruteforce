9918251e34897f00fdd9d8a02c2db71260e3b488
cioccarellia <andrea.cioccarelli01@gmail.com>
2024-02-28 17:54:34 +0100

    MIDA

--- core_api.py
+++ core_api.py
@@ -29,17 +29,33 @@ class DecryptionStatus(Enum):
     NOT_DECRYPTED = "NOT_DECRYPTED"
 
 
-class OriginalFileInfo:
-    def __init__(self, encrypted_filepath, decryption_status):
+class DecryptionProcessResult:
+    def __init__(self, encrypted_filepath, decryption_status, decryption_metadata):
         self.encrypted_filepath = encrypted_filepath
         self.decryption_status = decryption_status
+        self.decryption_metadata = decryption_metadata
 
+    def __str__(self):
+        return f"DecryptionProcessResult(" \
+               f"encrypted_filepath='{self.encrypted_filepath}', " \
+               f"decryption_status='{self.decryption_status}', " \
+               f"decryption_metadata={self.decryption_metadata})"
 
-class DecryptionResult:
-    def __init__(self, decrypted_filepath, password, elapsed_time_seconds):
+class DecryptedMetadata:
+    def __init__(self, decrypted_filepath, password, elapsed_time_seconds, attempts, search_space_size):
         self.decrypted_filepath = decrypted_filepath
         self.password = password
         self.elapsed_time_seconds = elapsed_time_seconds
+        self.attempts = attempts
+        self.search_space_size = search_space_size
+
+    def __str__(self):
+        return f"DecryptedMetadata(" \
+               f"decrypted_filepath='{self.decrypted_filepath}', " \
+               f"password='{self.password}', " \
+               f"elapsed_time_seconds={self.elapsed_time_seconds}, " \
+               f"attempts={self.attempts}, " \
+               f"search_space_size={self.search_space_size})"
 
 
 def decrypt_pdf(input_path, output_path, generator):
@@ -106,9 +122,20 @@ def decrypt_pdf(input_path, output_path, generator):
 
                         # Calculate elapsed time
                         elapsed_ms = current_milli_time() - initial_time_ms
+                        elapsed_s = elapsed_ms / 1000
 
-                        logging.info(f"[{input_path}]: Decryption successful, password={guess}. Decrypted file saved as {new_filename}. Took {elapsed_ms / 1000}s. Explored {guess_count} guesses out of {guess_space_size} ({guess_count/guess_space_size}% coverage)")
-                        return
+                        logging.info(f"[{input_path}]: Decryption successful, password={guess}. Decrypted file saved as {new_filename}. Took {elapsed_s}s. Explored {guess_count} guesses out of {guess_space_size} ({guess_count/guess_space_size}% coverage)")
+                        return DecryptionProcessResult(
+                            input_path,
+                            DecryptionStatus.DECRYPTED,
+                            DecryptedMetadata(
+                                new_filename,
+                                guess,
+                                elapsed_s,
+                                guess_count,
+                                guess_space_size
+                            )
+                        )
                     except FileNotDecryptedError as unsuccessful_decryption:
                         if param_verbose_output:
                             logging.warning(f"[{input_path}]: Decryption attempt unsuccessful: guess={guess}")
@@ -160,7 +187,7 @@ def decrypt_files_in_directory(directory, generator):
                 executor.submit(
                     decrypt_pdf,
                     os.path.join(directory, filename_pdf),
-                    os.path.join(directory, f"pypf_decrypted_{filename_pdf}"),
+                    os.path.join(directory, f"pybf_decrypted_{filename_pdf}"),
                     generator
                 ) for filename_pdf in dir_files_pdfs
             ]
@@ -179,7 +206,13 @@ def decrypt_files_in_directory(directory, generator):
 
             for future in future_tasks:
                 try:
-                    print(future.result())
+                    result = future.result()
+                    if result is not None:
+                        if result.decryption_status == DecryptionStatus.DECRYPTED:
+                            logging.info(result.decryption_metadata)
+                        else:
+                            logging.warning(result)
+
                 except Exception as future_error:
                     logging.error(f"Multithreading error: {future_error}")
     else:
@@ -190,7 +223,7 @@ def decrypt_files_in_directory(directory, generator):
 
                 # Define paths
                 encrypted_pdf_file_path = os.path.join(directory, filename_pdf)
-                decrypted_pdf_file_path = os.path.join(directory, f"pypbf_decrypted_{filename_pdf}")
+                decrypted_pdf_file_path = os.path.join(directory, f"pybf_decrypted_{filename_pdf}")
 
                 # Run Decryption
                 decrypt_pdf(encrypted_pdf_file_path, decrypted_pdf_file_path, generator)
